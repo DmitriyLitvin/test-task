a7a777be2e9d28447a0a6063240f90062071164d
dmytro.litvin <dmytro.litvin@avid.com>
2022-09-20 22:43:38 +0300

    small changes

--- src/main/java/org/example/CinemaApplication.java
+++ src/main/java/org/example/CinemaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.Transactional;
+
 
 
 @SpringBootApplication
